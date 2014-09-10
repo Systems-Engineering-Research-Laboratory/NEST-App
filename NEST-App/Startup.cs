143e98d0514da323cfb209fe2e63db25a5bcf5bb
Varatep Buranintu <vb.creations500@gmail.com>
2014-09-10 14:02:43 -0700

    staging commit

---
+++ NEST-App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NEST_App.Startup))]
+namespace NEST_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
