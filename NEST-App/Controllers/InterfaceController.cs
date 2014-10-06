61d08154f5015acd27d7dd7dffc219a334385001
Varatep Buranintu <vb.creations500@gmail.com>
2014-10-05 20:08:15 -0700

    adding google map

---
+++ NEST-App/Controllers/InterfaceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NEST_App.Controllers
+{
+    public class InterfaceController : Controller
+    {
+        // GET: Interface
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            return View();
+        }
+    }
+}
