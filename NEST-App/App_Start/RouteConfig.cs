b2006c636349d59b0bf698db786afe5a08c067b1
Varatep Buranintu <vb.creations500@gmail.com>
2014-10-05 20:13:48 -0700

    setting default routes and adding varatep user

--- NEST-App/App_Start/RouteConfig.cs
+++ NEST-App/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NEST_App
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Interface", action = "Map", id = UrlParameter.Optional }
             );
         }
     }
