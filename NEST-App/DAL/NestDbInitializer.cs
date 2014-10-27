b2f49362e831a6c22d9f00b12c00d2a006491952
Jeffrey Limbacher <jeffreylimbacher@gmail.com>
2014-10-26 21:58:55 -0700

    It should be set up to create a database every time the app is ran (I think). Right now, there is only 1 Vehicle in the database. I also added a couple fields to some of the models.

---
+++ NEST-App/DAL/NestDbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using NEST_App.Models;
+
+namespace NEST_App.DAL
+{
+    public class NestDbInitializer : System.Data.Entity.DropCreateDatabaseAlways<NestDbContext>
+    {
+        protected override void Seed(NestDbContext context)
+        {
+            var ownshipVehicles = new List<OwnshipVehicle>
+            {
+                new OwnshipVehicle{Callsign = "HAWK21", NumDeliveries = 2134, Mileage = 234}
+            };
+            ownshipVehicles.ForEach(s => context.OwnshipVehicles.Add(s));
+        }
+    }
+}
