90cb899ede1194e47c61eec284fa6e5773c2b1b5
bbobbo216 <bbobbo0918@gmail.com>
2014-11-11 05:53:27 +0900

    Merge branch 'master' of https://github.com/varatep/NEST-App
    Conflicts:
    	NEST-App/Web.config

---
+++ NEST-App/Controllers/FlightStateController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using NEST_App.DAL;
+using NEST_App.Models;
+
+namespace NEST_App.Controllers
+{
+    public class FlightStateController : ApiController
+    {
+        private NestDbContext db = new NestDbContext();
+
+        // GET: api/FlightState
+        public IQueryable<FlightState> GetFlightStates()
+        {
+            return db.FlightStates;
+        }
+
+        // GET: api/FlightState/5
+        [ResponseType(typeof(FlightState))]
+        public async Task<IHttpActionResult> GetFlightState(int id)
+        {
+            FlightState flightState = await db.FlightStates.FindAsync(id);
+            if (flightState == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(flightState);
+        }
+
+        // PUT: api/FlightState/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutFlightState(int id, FlightState flightState)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != flightState.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(flightState).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FlightStateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/FlightState
+        [ResponseType(typeof(FlightState))]
+        public async Task<IHttpActionResult> PostFlightState(FlightState flightState)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.FlightStates.Add(flightState);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = flightState.Id }, flightState);
+        }
+
+        // DELETE: api/FlightState/5
+        [ResponseType(typeof(FlightState))]
+        public async Task<IHttpActionResult> DeleteFlightState(int id)
+        {
+            FlightState flightState = await db.FlightStates.FindAsync(id);
+            if (flightState == null)
+            {
+                return NotFound();
+            }
+
+            db.FlightStates.Remove(flightState);
+            await db.SaveChangesAsync();
+
+            return Ok(flightState);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FlightStateExists(int id)
+        {
+            return db.FlightStates.Count(e => e.Id == id) > 0;
+        }
+    }
+}
--- NEST-App/Global.asax.cs
+++ NEST-App/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NEST_App.DAL;
 
 namespace NEST_App
 {
