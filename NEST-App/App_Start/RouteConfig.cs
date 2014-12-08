ed9a95d0adf0208c8a71093b92dc6db947ceb073
Varatep Buranintu <vb.creations500@gmail.com>
2014-12-08 15:11:59 -0800

    changing default startup to /Home/Index instead of /Interface/Index

--- NEST-App/App_Start/RouteConfig.cs
+++ NEST-App/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NEST_App
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Interface", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
