853f9080408fb14496bb23aa1877a32c5ead5adf
Dragos Guta <dragossg@gmail.com>
2015-01-15 11:41:08 -0800

    added start/stop and 'base' buttons to google map

--- NEST-App/Content/Map/GOOG/MapGOOG.js
+++ NEST-App/Content/Map/GOOG/MapGOOG.js
@@ -4,12 +4,49 @@ var uavIcon;
 var uavTrail;
 var trailArray = [];
 infoWindow = new google.maps.InfoWindow;
+var homeBase = new google.maps.LatLng(34.2417, -118.529);
 
+//Button for zooming to base on click
+function BaseControl(controlDiv, map) {
+    controlDiv.style.padding = '6px';
+
+    //CSS for control button exterior
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = 'white';
+    controlUI.style.borderStyle = 'solid';
+    controlUI.style.borderWidth = '1px';
+    controlUI.style.width = '53px';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = "Zoom to base";
+    controlDiv.appendChild(controlUI);
+
+    //CSS for control button interior
+    var controlText = document.createElement('div');
+    controlText.style.fontFamily = 'Arial,sans-serif';
+    controlText.style.fontSize = '11px';
+    controlText.style.paddingLeft = '4px';
+    controlText.style.paddingRight = '4px';
+    controlText.style.paddingTop = '2px';
+    controlText.style.paddingBottom = '4px';
+    controlText.innerHTML = 'Base';
+    controlUI.appendChild(controlText);
+
+    //Click event listener
+    google.maps.event.addDomListener(controlUI, 'click', function () {
+        map.setCenter(homeBase);
+        map.setZoom(18);
+    });
+}
+
+
+//Initialize the map with all the markers/features etc.
 function initialize() {
 
     var mapOptions = {
         zoom: 18,
-        center: new google.maps.LatLng(34.2417, -118.529)
+        center: new google.maps.LatLng(34.2417, -118.529),
+        disableDoubleClickZoom: true
     }
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
@@ -67,6 +104,12 @@ function initialize() {
         infoWindow.setPosition(ne);
         infoWindow.open(map);
     }
+
+    var homeControlDiv = document.createElement('div');
+    var homeControl = new BaseControl(homeControlDiv, map);
+
+    homeControlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.RIGHT].push(homeControlDiv);
     //End map initialize
 }
 
@@ -78,13 +121,30 @@ function placeTrail(location) {
     trailArray.push(marker);
 }
 
+function isMoving() {
+    for (i = 0; i < trailArray.length; i++) {
+        if (trailArray[i - 1] > trailArray[i])
+            return false;
+    }
+    return true;
+}
+
+function displayTrail() {
+    for (i = 0; i < trailArray.length; i++) {
+        trailArray[i].setMap(map);
+    }
+}
+
 $(document).ready(function () {
     google.maps.event.addDomListener(window, 'load', initialize);
     var vehicleHub = $.connection.vehicleHub;
     vehicleHub.client.flightStateUpdate = function (vehicle) {
+        console.log(vehicle); + " " + console.log(vehicle.Latitude); + " " + console.log(vehicle.Longitude);
         var LatLng = new google.maps.LatLng(vehicle.Latitude, vehicle.Longitude);
         uavMarker.setPosition(LatLng);
         placeTrail(LatLng);
+        isMoving.apply(this, trailArray);
+
         var contentString2 = '<b>UAV</b><br>' +
                 'Latitude: ' + vehicle.Latitude + '<br>' +
                 'Longitude: ' + vehicle.Longitude;
@@ -92,15 +152,10 @@ $(document).ready(function () {
 
         google.maps.event.addListener(uavMarker, 'click', function () {
             infoWindow.open(map, uavMarker);
-            for (i = 0; i < trailArray.length; i++) {
-                trailArray[i].setMap(map);
-            }
-
+           
         });
         google.maps.event.addListener(map, 'click', function () {
-            for (i = 0; i < trailArray.length; i++) {
-                trailArray[i].setMap(null);
-            }
+            
         });
     }
 });
