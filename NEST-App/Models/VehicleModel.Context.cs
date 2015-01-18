5a1dca1f17b344470c49eb23b3cf6f9c3e545105
Varatep Buranintu <varatep@buranintu.com>
2015-01-18 13:52:44 -0800

    edit the context generator

--- NEST-App/Models/VehicleModel.Context.cs
+++ NEST-App/Models/VehicleModel.Context.cs
@@ -7,34 +7,38 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using System;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-
-public partial class VehicleModelContainer : DbContext
+namespace NEST_App.Models
 {
-    public VehicleModelContainer()
-        : base("name=VehicleModelContainer")
+    using System;
+    using NEST_App.Models;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    
+    public partial class VehicleModelContainer : DbContext
     {
+        public VehicleModelContainer()
+            : base("name=VehicleModelContainer")
+        {
+        }
+    
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+    
+        public virtual DbSet<UAV> UAVs1 { get; set; }
+        public virtual DbSet<FlightState> FlightStates { get; set; }
+        public virtual DbSet<Configuration> Configurations { get; set; }
+        public virtual DbSet<Equipment> Equipments { get; set; }
+        public virtual DbSet<EquipmentHealth> EquipmentHealths { get; set; }
+        public virtual DbSet<NonownshipVehicle> NonownshipVehicles { get; set; }
+        public virtual DbSet<Schedule> Schedules { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Mission> Missions { get; set; }
+        public virtual DbSet<Maintenance> Maintenances { get; set; }
+        public virtual DbSet<MissionLog> MissionLogs { get; set; }
+        public virtual DbSet<MissionLogActivity> MissionLogActivities { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserRole> UserRoles { get; set; }
     }
-
-    protected override void OnModelCreating(DbModelBuilder modelBuilder)
-    {
-        throw new UnintentionalCodeFirstException();
-    }
-
-    public virtual DbSet<UAV> UAVs1 { get; set; }
-    public virtual DbSet<FlightState> FlightStates { get; set; }
-    public virtual DbSet<Configuration> Configurations { get; set; }
-    public virtual DbSet<Equipment> Equipments { get; set; }
-    public virtual DbSet<EquipmentHealth> EquipmentHealths { get; set; }
-    public virtual DbSet<NonownshipVehicle> NonownshipVehicles { get; set; }
-    public virtual DbSet<Schedule> Schedules { get; set; }
-    public virtual DbSet<Order> Orders { get; set; }
-    public virtual DbSet<Mission> Missions { get; set; }
-    public virtual DbSet<Maintenance> Maintenances { get; set; }
-    public virtual DbSet<MissionLog> MissionLogs { get; set; }
-    public virtual DbSet<MissionLogActivity> MissionLogActivities { get; set; }
-    public virtual DbSet<User> Users { get; set; }
-    public virtual DbSet<UserRole> UserRoles { get; set; }
 }
