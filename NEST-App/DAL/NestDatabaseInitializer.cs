4cc8a2429b061a1dfa96c4dbef631ce9c92a21d7
Varatep Buranintu <varatep@buranintu.com>
2015-01-18 13:45:39 -0800

    add my db init

---
+++ NEST-App/DAL/NestDatabaseInitializer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NEST_App.Models;
+
+namespace NEST_App.DAL
+{
+    public class NestDatabaseInitializer : System.Data.Entity.DropCreateDatabaseAlways<VehicleModelContainer>
+    {
+        protected override void Seed(VehicleModelContainer DBContext)
+        {
+            // We can initialize everything here and store it into the database
+            var uavs = new List<UAV>
+            {
+            };
+            //for (int i = 0; i < 500; i++)
+            //{
+            //    uavs.Add(new UAV 
+            //    { 
+            //        Callsign= "HAWK" + i.ToString(),
+            //        Configurations= ,
+            //        create_date='',
+            //        EuipmentHealths='',
+            //        FlightStates='',
+            //        Mileage='',
+            //        modified_date='',
+            //        NumDeliveries='',
+            //        Schedules=''
+            //    });
+            //}
+
+            // loop through and add UAV x 500
+            // then add the required objects
+
+
+            // not needed
+            //base.Seed(context);
+        }
+    }
+}
