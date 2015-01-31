582767d5b4f24ec2f9ae60db2947a95245740bf9
Varatep Buranintu <varatep@buranintu.com>
2015-01-30 23:39:42 -0800

    finish front end event log

--- NEST-App/Scripts/app/eventlog.js
+++ NEST-App/Scripts/app/eventlog.js
@@ -2,13 +2,35 @@
 
 eventlogapp
 
-.controller('EventLogCtrl', ['$scope', 'eventhub', function ($scope, eventhub) {
+.controller('EventLogCtrl', ['$scope', 'Hub', function ($scope, Hub) {
     $scope.events = [];
+    var hub = new Hub('event', {
+        listeners: {
+            'emitevent': function (evt) {
+                if (typeof evt.uav_id != "undefined") {
+                    $scope.events.push(evt);
+                }
+            }
+        },
+        errorHandler: function (err) {
+            console.log(err);
+        }
+    });
     
 }])
-.factory('eventhub', ['$rootScope', 'Hub', function ($rootScope, Hub) {
-    var eventhub = {};
-    return {
+//.factory('eventhub', ['$rootScope', 'Hub', function ($rootScope, Hub) {
+//    var hub = new Hub('event', {
+//        listeners: {
+//            'emitevent': function (evt) {
+//            }
+//        },
+//        errorHandler: function (err) {
+//            console.log(err);
+//        }
+//    });
+//    return {
+//        newEvent: function (evt) {
 
-    }
-}])
+//        }
+//    }
+//}])
