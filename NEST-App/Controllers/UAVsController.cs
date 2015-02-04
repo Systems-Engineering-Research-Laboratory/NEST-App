08ea99e7261436f3e0623072bf0d3e4762047b78
Dragos Guta <dragossg@gmail.com>
2015-02-03 20:39:55 -0800

    removed auto generated api, added attribute routing

--- NEST-App/Controllers/UAVsController.cs
+++ NEST-App/Controllers/UAVsController.cs
@@ -7,9 +7,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web.Http;
+using System.Web.Http; 
 using System.Web.Http.Description;
-using System.Web.Mvc;
 using System.Web;
 using NEST_App.DAL;
 using NEST_App.Models;
@@ -21,6 +20,8 @@ namespace NEST_App.Controllers.Api
         private NestContainer db = new NestContainer();
 
         //GET: api/uavs/getuavinfo
+         [Route("api/uavs/getuavinfo")]
+        [System.Web.Mvc.HttpGet]
         public HttpResponseMessage GetUAVInfo()
         {
             var uavs = from u in db.UAVs.Include(u => u.FlightStates).Include(u => u.Schedules)
@@ -48,105 +49,6 @@ namespace NEST_App.Controllers.Api
                            FlightState = u.FlightStates.OrderBy(fs => fs.Timestamp).FirstOrDefault(),
                        };
             return Request.CreateResponse(HttpStatusCode.OK, uavs);
-        }
-
-        // GET: api/UAVs
-        public IQueryable<UAV> GetUAVs()
-        {
-            return db.UAVs;
-        }
-
-        // GET: api/UAVs/5
-        [ResponseType(typeof(UAV))]
-        public async Task<IHttpActionResult> GetUAV(int id)
-        {
-            UAV uAV = await db.UAVs.FindAsync(id);
-            if (uAV == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(uAV);
-        }
-
-        // PUT: api/UAVs/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutUAV(int id, UAV uAV)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != uAV.Id)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(uAV).State = System.Data.Entity.EntityState.Modified;
-
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UAVExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/UAVs
-        [ResponseType(typeof(UAV))]
-        public async Task<IHttpActionResult> PostUAV(UAV uAV)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.UAVs.Add(uAV);
-            await db.SaveChangesAsync();
-
-            return CreatedAtRoute("DefaultApi", new { id = uAV.Id }, uAV);
-        }
-
-        // DELETE: api/UAVs/5
-        [ResponseType(typeof(UAV))]
-        public async Task<IHttpActionResult> DeleteUAV(int id)
-        {
-            UAV uAV = await db.UAVs.FindAsync(id);
-            if (uAV == null)
-            {
-                return NotFound();
-            }
-
-            db.UAVs.Remove(uAV);
-            await db.SaveChangesAsync();
-
-            return Ok(uAV);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool UAVExists(int id)
-        {
-            return db.UAVs.Count(e => e.Id == id) > 0;
         }
     }
 }
