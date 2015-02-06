9319252e7ada46c32731d860613aa5c5ce89970c
Varatep Buranintu <varatep@buranintu.com>
2015-02-05 22:42:32 -0800

    createusers api controller with basic crud and modelstate checks. begin angular app for admin page.

---
+++ NEST-App/Controllers/app/js/admin_page/controllers/admin.ctrl.js
@@ -0,0 +1,38 @@
+﻿angular.module('adminApp').controller('AdminCtrl', ['$scope', function ($scope) {
+    $scope.init = function () {
+        $scope.newUser = {};
+        $scope.newVehicle = {};
+        $scope.allUsers = [];
+        $scope.allVehicles = [];
+        $scope.currentAdmin = {};
+
+        var User = function (user) {
+            //insert user model here
+            var User = {
+
+            };
+        };
+        var Vehicle = function (vehicle) {
+            //insert vehicle model here
+            var Vehicle = {
+
+            };
+        };
+
+        $scope.createUser = function () {
+            $http({
+                method: 'POST',
+                url: '/api/users',
+                data: $scope.newUser
+            });
+        };
+        $scope.createVehicle = function () {
+            $http({
+                method: 'POST',
+                url: '/api/users',
+                data: $scope.newVehicle
+            });
+        };
+    };
+    $scope.init();
+}])
