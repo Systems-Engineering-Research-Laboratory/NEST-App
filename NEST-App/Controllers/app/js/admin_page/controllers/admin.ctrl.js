d8b440b03f80d6c354995a0c8f0b3ccd6ce0bc6d
Varatep Buranintu <varatep@buranintu.com>
2015-02-08 17:15:20 -0800

    angular uav controlling

--- NEST-App/Controllers/app/js/admin_page/controllers/admin.ctrl.js
+++ NEST-App/Controllers/app/js/admin_page/controllers/admin.ctrl.js
@@ -1,11 +1,12 @@
-﻿angular.module('adminApp').controller('AdminCtrl', ['$scope', function ($scope) {
+﻿angular.module('adminApp').controller('AdminCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.init = function () {
         $scope.newUser = {};
         $scope.newVehicle = {};
         $scope.allUsers = [];
         $scope.allVehicles = [];
         $scope.currentAdmin = {};
-
+        $scope.uavs = [];
+        $scope.desiredUAVCount = '';
         var User = function (user) {
             //insert user model here
             var User = {
@@ -37,6 +38,35 @@
                 success: function (data) {
                     $scope.allVehicles.push(data);
                 }
+            });
+        };
+        $scope.generateUAVs = function () {
+            //$http({
+            //    method: 'POST',
+            //    url: '/api/uavs/generateuavs/' + count,
+            //    success: function (data) {
+            //        $scope.uavs = (data);
+            //    },
+            //    error: function (err) {
+            //        alert(err);
+            //    }
+            //});
+            $http.post('/api/uavs/generateuavs', { number: $("#desiredUAVCount").val() })
+            .success(function (data, status, headers, config) {
+
+            })
+            .error(function (data, status, headers, config) {
+
+            });
+        };
+        $scope.getGeneratedUAVs = function () {
+            $http.get('/api/uavs/getgenerateduavs')
+            .success(function (data, status, headers, config) {
+                $scope.uavs = data;
+                //console.log(data);
+            })
+            .error(function (data, status, headers, config) {
+                alert(data);
             });
         };
     };
