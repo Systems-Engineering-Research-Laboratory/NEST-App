4252e5b8d9a9a1b61dba098f4b54efb7708265a5
Jeffrey Limbacher <jeffreylimbacher@gmail.com>
2015-02-14 18:05:34 -0800

    Created new waypoint manager which will draw waypoints on the map. Moved the sim scripts into a new folder.

---
+++ NEST-App/Content/Map/GOOG/WaypointManager.js
@@ -0,0 +1,105 @@
+﻿
+function WaypointManager() {
+    this.schedules = [];
+    this.markers = [];
+    var that; //that's the stuff
+    $.ajax({
+        url: '/api/schedule',
+        type: 'GET'
+    }).success(function (data, textStatus, jqXHR) {
+        this.schedules = data;
+    });
+
+    this.addMarker = function (marker) {
+        google.maps.event.addListener(marker, 'icon_changed', function () {
+            that.handleIconChanged(this);
+        });
+        this.markers.push(marker);
+    }
+
+    this.handleIconChanged = function (marker) {
+        if (!marker.uav) {
+            return;
+        }
+        var uav = marker.uav;
+        var Id = uav.Id;
+        var sched = this.getScheduleByUavId(Id);
+        if (sched == null || !sched.CurrentMission) {
+            return;
+        }
+        if (marker.icons.fillColor === "green") {
+            this.displayWaypointsPerMission(sched.CurrentMission)
+        }
+        else {
+            //TODO: implement this function
+            //this.hideWaypointsPerMission(sched.CurrentMission);
+        }
+    }
+
+    this.getScheduleById = function (id) {
+        for (var i = 0; i < this.schedules.length; i++) {
+            if (schedules[i].UAVId == id) {
+                return schdules[i].UAVId
+            }
+        }
+        return null;
+    }
+
+    this.displayWaypointsPerMission = function (mission) {
+        var wps = mission.Waypoints;
+        for (var i = 0; i < wps.length; i++) {
+            var wp = wps[i];
+            if (wp.circle) {
+                wp.circle.setMap(this.map);
+            }
+            else {
+                var ll = new google.maps.LatLng(wp.Latitude, wp.Longitude);
+                var wpOptions = {
+                    center: ll,
+                    map: this.map,
+                    strokeColor: '#0000FF', //blue
+                    strokeOpacity: 0.8,
+                    fillColor: '#0000FF',
+                    fillOpacity: 0.5,
+                    radius: 10,
+                }
+                mission.point = new google.maps.Circle(wpOptions);
+            }
+        }
+        if (mission.flightPath) {
+            mission.flightPath.setMap(this.map)
+        }
+        else if (wps.length > 1) {
+            var curWp = wps[0];
+            var points = [new google.maps.LatLng(curWp.Latitude, curWp.Longitude)]
+            while (curWp.NextWaypointId) {
+                var nextWp = findWaypointById(id, wps);
+                if (nextWp) {
+                    var next = new googlemaps.LatLng(nextWp.Latitude, nextWp.Longitude);
+                    points.push(next);
+                    curWp = nextWp;
+                }
+                else {
+                    //Avoid null pointer
+                    break;
+                }
+            }
+            var flightPath = new google.maps.Polyline({
+                path: points,
+                strokeColor: '#FF0000',
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+            });
+            flightPath.setMap(flightPath);
+        }
+    }
+}
+
+function findWaypointById(id, wps) {
+    for (var i = 0; i < wps.length; i++) {
+        if (wps[i].Id == id) {
+            return wps[i];
+        }
+    }
+    return null;
+}
---
+++ NEST-App/Scripts/NestScripts/reporter.js
@@ -0,0 +1,83 @@
+﻿//Wrapper object that wraps up communications to the server. A pretty leaky abstraction though.
+//Something require callbacks leaking the fact that values cannot be returned immediately. Oh well!
+function Reporter() {
+    this.hub = $.connection.vehicleHub;
+
+    this.pendingResult = false;
+
+    this.updateMission = function (mission, opts) {
+        var jqXHR = this.putToServer(
+            '/api/missions/' + mission.id,
+            {
+                Phase: mission.Phase,
+                FlightPattern: mission.FlightPattern,
+                Payload: mission.Payload,
+                Priority: mission.Priority,
+                FinancialCost: mission.FinancialCost,
+                TimeAssigned: mission.TimeAssigned,
+                TimeComplete: mission.TimeCompleted,
+                DestinationCoordinates: mission.DestinationCoordinates.Geography.WellKnownText,
+                ScheduledCompletionTime: mission.ScheduledCompletionTime,
+                EstimatedCompletionTime: mission.EstimatedCompletionTime,
+                id: mission.id,
+                ScheduleId: mission.ScheduleId,
+                create_date: mission.create_date,
+                modified_date: mission.modified_date,
+            },
+            {});
+
+    }
+
+    this.updateFlightState = function (fs, opts) {
+        this.hub.server.pushFlightStateUpdate(fs);
+    }
+
+    this.putToServer = function (url, data, opts, success) {
+
+        return $.ajax({
+            url: url,
+            data: JSON.stringify(data),
+            dataType: 'json',
+            success: success,
+            type: 'PUT',
+            contentType: "application/json",
+        });
+    }
+
+    this.ackCommand = function (cmd, type, reason, accepted) {
+        this.hub.server.ackCommand({
+            CommandId: cmd.Id,
+            CommandType: type,
+            Reason: reason,
+            Accepted: accepted
+        }, cmd.connId);
+    }
+
+    this.retrieveWaypointsByMissionId = function (id, caller, success) {
+        this.pendingResult = false;
+        var url = '/api/missions/waypoints/' + id;
+        return $.ajax({
+            url: url,
+            success: function (data, textStatus, jqXHR) { success(data, textStatus, jqXHR); }
+        });
+    }
+
+    this.insertWaypoint = function (id, newWp) {
+        this.pendingResult = false;
+        var url = '/api/waypoints/insert/' + id;
+        return $.ajax({
+            type: "POST",
+            url: url,
+            data: newWp,
+        });
+    }
+
+    this.updateWaypoint = function (wp) {
+        var url = '/api/waypoints/' + wp.Id;
+        return $.ajax({
+            type: 'PUT',
+            url: url,
+            data: wp
+        });
+    }
+}
