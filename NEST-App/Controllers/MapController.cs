e397029026884345baeafd6d6c12bd9f0de28e6e
Jeffrey Limbacher <jeffreylimbacher@gmail.com>
2015-03-01 22:09:21 -0800

    Forgot a file in last commit

--- NEST-App/Controllers/MapController.cs
+++ NEST-App/Controllers/MapController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NEST_App.Models;
 
 namespace NEST_App.Controllers
 {
     public class MapController : Controller
     {
+
+        private NestContainer db = new NestContainer();
         //
         // GET: /Map/Overview
         public ActionResult Overview()
@@ -18,6 +21,30 @@ namespace NEST_App.Controllers
         // GET: /Map/GoogleMap
         public ActionResult GoogleMap()
         {
+            string name = (string)System.Web.HttpContext.Current.Cache["current_user"];
+
+            if(name != null)
+            {
+                var curUser = (from u in db.Users
+                               where u.username.Equals(name)
+                              select new
+                              {
+                                  username = u.username,
+                                  user_id = u.user_id,
+                              }).FirstOrDefault();
+
+                if (curUser != null)
+                {
+                    var assignedUavs = (from u in db.UAVs
+                                       where u.User_user_id == curUser.user_id
+                                        select u.Id).ToList();
+
+
+                    ViewBag.currentUser = curUser;
+                    ViewBag.assignedUavs = assignedUavs;
+                }
+            }
+
             return View();
         }
 	}
