ea09d07c4d2a907e3bfe063c448306550384c95a
Varatep Buranintu <varatep@buranintu.com>
2015-04-07 10:49:03 -0700

    fix connection string hahahahah

--- NEST-App/Models/VehicleModel.edmx.sql
+++ NEST-App/Models/VehicleModel.edmx.sql
@@ -2,16 +2,10 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 04/07/2015 10:40:01
+-- Date Created: 04/07/2015 10:47:49
 -- Generated from EDMX file: C:\Users\Varatep-mac\Documents\Visual Studio 2013\Projects\NEST-App\NEST-App\Models\VehicleModel.edmx
 -- --------------------------------------------------
 
-SET QUOTED_IDENTIFIER OFF;
-GO
-USE [NEST-Staging];
-GO
-IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
-GO
 
 -- --------------------------------------------------
 -- Dropping existing FOREIGN KEY constraints
