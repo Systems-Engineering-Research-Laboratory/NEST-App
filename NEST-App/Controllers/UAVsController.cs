50a841dbf8e5d9c8d20f408076438a8f1bc52982
Varatep Buranintu <varatep@buranintu.com>
2015-04-25 21:47:13 -0700

    geocode methods

--- NEST-App/Controllers/UAVsController.cs
+++ NEST-App/Controllers/UAVsController.cs
@@ -21,6 +21,33 @@ using NEST_App.Hubs;
 
 namespace NEST_App.Controllers.Api
 {
+    public static class GeoCodeCalc
+    {
+        public const double EarthRadiusInMiles = 3956.0;
+        public const double EarthRadiusInKilometers = 6367.0;
+
+        public static double ToRadian(double val) { return val * (Math.PI / 180); }
+        public static double DiffRadian(double val1, double val2) { return ToRadian(val2) - ToRadian(val1); }
+
+        public static double CalcDistance(double lat1, double lng1, double lat2, double lng2)
+        {
+            return CalcDistance(lat1, lng1, lat2, lng2, GeoCodeCalcMeasurement.Miles);
+        }
+
+        public static double CalcDistance(double lat1, double lng1, double lat2, double lng2, GeoCodeCalcMeasurement m)
+        {
+            double radius = GeoCodeCalc.EarthRadiusInMiles;
+
+            if (m == GeoCodeCalcMeasurement.Kilometers) { radius = GeoCodeCalc.EarthRadiusInKilometers; }
+            return radius * 2 * Math.Asin(Math.Min(1, Math.Sqrt((Math.Pow(Math.Sin((DiffRadian(lat1, lat2)) / 2.0), 2.0) + Math.Cos(ToRadian(lat1)) * Math.Cos(ToRadian(lat2)) * Math.Pow(Math.Sin((DiffRadian(lng1, lng2)) / 2.0), 2.0)))));
+        }
+    }
+
+    public enum GeoCodeCalcMeasurement : int
+    {
+        Miles = 0,
+        Kilometers = 1
+    }
     public class UAVsController : ApiController
     {
         private readonly NestContainer _db = new NestContainer();
@@ -50,7 +77,7 @@ namespace NEST_App.Controllers.Api
 
             if (currentMission.TimeAssigned == null) return new DateTime();
             var timeToMissionEnd = 
-                currentMission.ScheduledCompletionTime = ((DateTime) currentMission.TimeAssigned).AddMinutes(25);
+                currentMission.ScheduledCompletionTime = ((DateTime) currentMission.TimeAssigned).AddMinutes(currentMission.FlightPattern. / uav.MaxVelocity);
             _db.Entry(currentMission).State = System.Data.Entity.EntityState.Modified;
             _db.SaveChanges();
             return (DateTime) currentMission.ScheduledCompletionTime;
